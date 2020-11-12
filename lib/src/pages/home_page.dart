a144bc388d239cbcdadca3b0786651c4f4cbdad9
Freddy <freddy.artea@gmail.com>
2020-11-12 14:19:08 -0500

    Bottom Navigation Bar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,11 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('Hola Home Page'),
-    ));
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+    );
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      // le dice que elemento es el activo
+      currentIndex: currentIndex,
+      // se dispara cuando seleccione
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      // items obligatorios mas de 1
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Mapas'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), label: 'Direcciones')
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
   }
 }
